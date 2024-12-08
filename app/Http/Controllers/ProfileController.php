57d9afe5096c3e711161c7fe334bb01b9250ec20
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-12-09 01:14:04 +0600

    index function is created to set cookies

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function index(Request $request){
+        $name = "Donald Trump";
+        $age = "75";
+        $id = $request->id;
+        $data = [
+            "id"=>$id,
+            "name"=>$name,
+            "age"=>$age
+        ];
+        $name = "access_token";
+
+        $value = "123-XYZ";
+
+        $minutes = 1;
+
+        $path = "/";
+
+
+        $domain = $_SERVER["SERVER_NAME"];
+
+        $secure = false;
+
+        $httpOnly = true;
+
+        $cookie = cookie($name, $value, $minutes,$path,$domain,$secure,$httpOnly); 
+
+        return response($data)->cookie($cookie);
+    }
 }
